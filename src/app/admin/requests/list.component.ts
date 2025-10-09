bc4e2896eb9e45850e0fce21e20ee28e2f1f39f0
Geroy488 <mayecha302@gmail.com>
2025-10-09 15:56:45 +0800

    for te requests to show

--- src/app/admin/requests/list.component.ts
+++ src/app/admin/requests/list.component.ts
@@ -1,23 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 import { RequestsService } from '@app/_services/requests.service';
+import { AccountService } from '@app/_services/account.service';
+import { Account } from '@app/_models/account';
+import { Employee } from '@app/_models/employee';
 
 @Component({
   selector: 'app-requests-list',
-  templateUrl: './list.component.html'
+  templateUrl: 'list.component.html'
 })
-export class RequestsListComponent implements OnInit {
+export class ListComponent implements OnInit {
   requests: any[] = [];
+  currentUser!: Account | null;
 
-  constructor(private requestsService: RequestsService) {}
+  constructor(
+    private requestsService: RequestsService,
+    private accountService: AccountService
+  ) {}
 
-  loading = true;
-  
   ngOnInit() {
-    this.requestsService.getAll()
+    this.accountService.account.subscribe({
+      next: user => {
+        this.currentUser = user;
+        if (user && user.employeeId) {
+          // 👇 Show only their own requests
+          this.loadRequests(user.employeeId);
+        } else {
+          console.warn('No employeeId found for current user');
+        }
+      }
+    });
+  }
+
+  loadRequests(employeeId: number) {
+    this.requestsService
+      .getByEmployeeId(employeeId)
       .pipe(first())
-      .subscribe((data: any[]) => {
-        this.requests = data;
+      .subscribe({
+        next: requests => (this.requests = requests),
+        error: err => console.error('Error loading requests:', err)
       });
   }
 }
